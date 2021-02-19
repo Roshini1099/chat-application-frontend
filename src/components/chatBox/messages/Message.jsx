bcb1c492bc2ea18d3620e77e667c440523e38e19
ashish.aryan@hyperverge.co <ashish.aryan@hyperverge.co>
2021-02-19 13:16:02 +0530

    style: changes the message box styling

--- src/components/chatBox/messages/Message.jsx
+++ src/components/chatBox/messages/Message.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 // import MessageHeader from './MessageHeader';
 import MessageContent from './MessageContent';
+import { useDispatch, useSelector } from 'react-redux';
 import { MuseumSharp } from '@material-ui/icons';
 
-const Messages = () => {
+const Messages = () =>
+{
+	const currentMessage = useSelector(state => state.currentChat)
+	console.log(currentMessage)
 	return (
+
 		<div>
-			<MessageContent />
+			{currentMessage.currentchat.messages.map((value, key) => (
+				<MessageContent key={key} data={value} />
+			))}
+
 		</div>
 	);
 };
